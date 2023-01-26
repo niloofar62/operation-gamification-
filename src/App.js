4c4d354180a1bb4784e97cf11f636521abd9a173
Kindra <kindra.kathleen@gmail.com>
2023-01-26 15:11:09 -0500

    app.js updated to include all components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import LeaderBoard from "./components/LeaderBoard";
+import Crown from "./components/Crown";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <LeaderBoard />
+      <Crown />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
