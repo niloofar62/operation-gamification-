91be6d480a85666910585971c4f724b4695e1e5f
n2020h <uzame.va@gmail.com>
2023-02-03 17:09:17 -0500

    added react router so new pages can be added

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>This is a page about us</p>
+      <h3>Contact Us</h3>
+      <p>Phone: 555-555-5555</p>
+    </div>
+  );
+}
+export default About;
