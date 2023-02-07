df5645b4950b6e17944919f872a34999162061b0
Niloofar Nayebi <nil_nyb@yahoo.com>
2023-02-07 12:01:26 -0500

    peoplelist's logic changhed

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -3,23 +3,72 @@ import "./PeopleList.css";
 
 function PeopleList({ data }) {
   const people = data.map((person) => {
-    let jewelsEarned = 0;
+    let jowelsEarned = 0;
     const jewels = person.scores.filter((score) => score > 8);
-    jewelsEarned = jewels.length;
+    jowelsEarned = jewels.length;
     console.log(person.name, jewels);
+
+    let images = [];
+    while (jowelsEarned >= 0) {
+      if (jowelsEarned >= 5) {
+        images.push(
+          <img
+            // src="path/to/five-or-more-jowels-image.png"
+            src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/066/351/original/5jewelCrown.png?1675786668"
+            alt="five or more jowels"
+          />
+        );
+        jowelsEarned -= 5;
+      } else if (jowelsEarned === 4) {
+        images.push(
+          //  <img src="path/to/four-jowels-image.png" alt="four jowels" />
+          <img
+            src="https://adad18.slack.com/files/U03SXQ4J8LD/F04NUEHP2QG/4_jewel_crown.svg"
+            alt="four jowels"
+          />
+        );
+        break;
+      } else if (jowelsEarned === 3) {
+        images.push(
+          // <img src="path/to/three-jowels-image.png" alt="three jowels" />
+          <img
+            src="https://adad18.slack.com/files/U03SXQ4J8LD/F04N77X5DV2/3_jewel_crown.svg"
+            alt="three jowels"
+          />
+        );
+        break;
+      } else if (jowelsEarned === 2) {
+        images.push(
+          // <img src="path/to/two-jowels-image.png" alt="two jowels" />
+          <img
+            src="https://adad18.slack.com/files/U03SXQ4J8LD/F04NUEHS62C/2_jewel_crown.svg"
+            alt="two jowels"
+          />
+        );
+        break;
+      } else if (jowelsEarned === 1) {
+        images.push(
+          // <img src="path/to/zero-jowels-image.png" alt="zero jowels" />
+          <img
+            src="https://adad18.slack.com/files/U03SXQ4J8LD/F04NHCX73S5/1_jewel_crown.svg"
+            alt="one jowels"
+          />
+        );
+        break;
+      } else if (jowelsEarned === 0) {
+        images.push(
+          <img
+            src="https://adad18.slack.com/files/U03SXQ4J8LD/F04NHCX73S5/1_jewel_crown.svg"
+            alt="zero jowels"
+          />
+        );
+        break;
+      }
+    }
 
     return (
       <div className="person-container" key={person.id}>
-        {Array.from({ length: jewelsEarned }).map(() => "💎")}
-        {/* {jewelsEarned > 0 && <p className="jowel">💎</p>} */}
-        {/* {jewelsEarned >= 5 && <p className="extra-crown">👑</p>} */}
-        {jewelsEarned >= 5 && (
-          <div className="extra-crown">
-            {Array.from({ length: Math.floor(jewelsEarned / 5) }, (_, i) => (
-              <p key={i}>👑</p>
-            ))}
-          </div>
-        )}
+        {images}
         <p className="crown">👑</p>
         <p className="name">{person.name}</p>
       </div>
